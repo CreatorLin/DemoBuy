f6582b160f62613fd3e93693d5abd4685e50d55f
Creator <Creator@home>
2016-12-12 07:54:08 +0800

    Add Tkec data model

---
+++ DemoBuy/Seller/Tkec/Ontimesale.cs
@@ -0,0 +1,12 @@
+﻿namespace DemoBuy.Seller.Tkec
+{
+    public class OnTimeSale
+    {
+        public string Href { get; set; }
+        public string Img { get; set; }
+        public string Text01 { get; set; }
+        public string Text02 { get; set; }
+        public string PnPrice { get; set; }
+        public string Price { get; set; }
+    }
+}
