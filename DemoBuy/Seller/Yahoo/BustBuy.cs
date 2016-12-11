09e12f0022e57f524b0b6451fa44c1763c0c1e6e
Creator <Creator@home>
2016-12-12 07:53:05 +0800

    Add Yahoo data mdoelAdd Yahoo data mdoel

---
+++ DemoBuy/Seller/Yahoo/BustBuy.cs
@@ -0,0 +1,14 @@
+﻿namespace DemoBuy.Seller.Yahoo
+{
+    public class BustBuy
+    {
+        public string Href { get; set; }
+        public string Alt { get; set; }
+        public string Img { get; set; }
+        public string DescTitle { get; set; }
+        public string NameTitle { get; set; }
+        public string[] DescList { get; set; }
+        public string SrPrice { get; set; }
+        public string SpPrice { get; set; }
+    }
+}
