467046622b2dfea10d9d3803843e24c308bd6251
Creator <Creator@home>
2016-12-12 10:06:56 +0800

    Add Pchome Seller

---
+++ DemoBuy/Seller/Pchome/PchomeSeller.cs
@@ -0,0 +1,31 @@
+﻿using HtmlAgilityPack;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace DemoBuy.Seller.Pchome
+{
+    static public class PchomeSeller
+    {
+        static public Welcome Data {
+            get
+            {
+                var onsaleUri = "http://24h.pchome.com.tw/onsale/v2";
+
+                const string XPATH = @"//script";
+                var path = new HtmlWeb().Load(onsaleUri).DocumentNode.SelectNodes(XPATH)
+                                    .FirstOrDefault(p => p.Attributes["src"]?.Value.StartsWith("/cdnm") ?? false)
+                                    .Attributes["src"].Value;
+
+                var dataUri = $"http://24h.pchome.com.tw{path}";
+                var dataStr = new WebClient().DownloadString(dataUri);
+
+                const string PATTERN = "WelcomeJson = \\[(\\{.{1,9999999}\\})\\];[\b]*var";
+                var json = Regex.Match(dataStr, PATTERN).Groups[1].Value;
+                
+                return JsonConvert.DeserializeObject<Welcome>(json);
+            }
+        }
+    }
+}
